64cfd2fa5ecde86232f61e14e9e8f6604b5c3249
dalpoom-crypto <dalpoom@gmail.com>
2026-03-05 15:22:19 +0900

    Delete public directory

--- public/admin-config.js
+++
@@ -1,9 +0,0 @@
-// 빌드 시 scripts/generate-admin-config.js 에 의해 생성됨. 환경 변수(VITE_FIREBASE_*) 사용.
-window.RUNLOG_ADMIN_FIREBASE_CONFIG = {
-  "apiKey": "AIzaSyA4SGIF6Yl4nTnddt8sCG7zQyMp87XeuOk",
-  "authDomain": "run-log-31420.firebaseapp.com",
-  "projectId": "run-log-31420",
-  "storageBucket": "run-log-31420.firebasestorage.app",
-  "messagingSenderId": "325067679087",
-  "appId": "1:325067679087:web:727201211a34ac6c1fb49a"
-};
